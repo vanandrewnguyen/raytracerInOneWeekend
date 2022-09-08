15b0f5742a373c4a943e2a1dd145a25b6b39f0b7
Van Andrew <andrew.nguyen25@outlook.com>
2022-09-08 16:41:19 +1000

    Temp

---
+++ Hitables/xzRect.h
@@ -0,0 +1,12 @@
+#ifndef XZRECT
+#define XZRECT
+
+#include "hitable.h"
+#include "../Materials/material.h"
+#include "../ray.h"
+
+class XZRect : public Hitable {
+
+}
+
+#endif
---
+++ Hitables/yzRect.h
@@ -0,0 +1,12 @@
+#ifndef YZRECT
+#define YZRECT
+
+#include "hitable.h"
+#include "../Materials/material.h"
+#include "../ray.h"
+
+class YZRect : public Hitable {
+
+}
+
+#endif
