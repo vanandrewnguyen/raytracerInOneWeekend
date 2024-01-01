62d86cfa1b261225169810f94ba4e5a4c47fa18f
Van Andrew <andrew.nguyen25@outlook.com>
2024-01-01 16:32:08 +1100

    Added normal maps to dielectric and lambertian materials.

--- Materials/dielectric.h
+++ Materials/dielectric.h
@@ -14,8 +14,16 @@ public:
 
 	// Change our scattering
 	virtual bool scatter(const Ray& rayIn, const hitRecord& rec, vec3& attenuation, Ray& scatteredRay) const {
+		// Scatter normal based on normal map
+		vec3 normal = rec.normal;
+		if (hasNormalMap) {
+			std::shared_ptr<NormalBase> nmap = normalMap;
+			vec3 perturbation = nmap->computeNormalShift(normal, rec.u, rec.v);
+			normal = nmap->shiftNormal(normal, perturbation);
+		}
+		
 		vec3 outwardNormal;
-		vec3 reflected = reflect(rayIn.getDirection(), rec.normal);
+		vec3 reflected = reflect(rayIn.getDirection(), normal);
 		float ior;
 
 		// Default 
@@ -24,16 +32,16 @@ public:
 		float reflectProb;
 		float cosine;
 
-		if (dot(rayIn.getDirection(), rec.normal) > 0) {
-			outwardNormal = -rec.normal;
+		if (dot(rayIn.getDirection(), normal) > 0) {
+			outwardNormal = -normal;
 			ior = refractionIndex;
-			cosine = dot(rayIn.getDirection(), rec.normal) / rayIn.getDirection().length();
+			cosine = dot(rayIn.getDirection(), normal) / rayIn.getDirection().length();
 			cosine = sqrt(1 - refractionIndex * refractionIndex * (1 - cosine * cosine));
 		} else {
 			// If we're exiting the volume we need to do the calculations backwards, from e.g. glass back to air
-			outwardNormal = rec.normal;
+			outwardNormal = normal;
 			ior = 1.0 / refractionIndex;
-			cosine = -dot(rayIn.getDirection(), rec.normal) / rayIn.getDirection().length();
+			cosine = -dot(rayIn.getDirection(), normal) / rayIn.getDirection().length();
 		}
 
 		if (refract(rayIn.getDirection(), outwardNormal, ior, refracted)) {
--- Materials/lambertian.h
+++ Materials/lambertian.h
@@ -17,12 +17,20 @@ public:
 
 	// Change our scattering
 	virtual bool scatter(const Ray& rayIn, const hitRecord& rec, vec3& attenuation, Ray& scatteredRay) const {
+		// Scatter normal based on normal map
+		vec3 normal = rec.normal;
+		if (hasNormalMap) {
+			std::shared_ptr<NormalBase> nmap = normalMap;
+			vec3 perturbation = nmap->computeNormalShift(normal, rec.u, rec.v);
+			normal = nmap->shiftNormal(normal, perturbation);
+		}
+
 		// Scatter our ray in random unit sphere
-		vec3 target = rec.pos + rec.normal + randInUnitSphere();
+		vec3 target = rec.pos + normal + randInUnitSphere();
 		
 		// Catch degenerate scatter direction (if you accidently generate the -normal vector, the sum will be zero hence math error)
 		if (target.nearZero()) {
-			target = rec.normal;
+			target = normal;
 		}
 		
 		scatteredRay = Ray(rec.pos, target - rec.pos, rayIn.getTime());
