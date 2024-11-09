ec84138e36313e5ae43f0fcb236c6a7e8f9d27be
Van Andrew <andrew.nguyen25@outlook.com>
2024-11-09 15:01:12 +1100

    First draft of the denoising algorithm finished. Blurs and does not preserve edges.

--- Render/render.h
+++ Render/render.h
@@ -228,6 +228,77 @@ namespace render {
             std::cerr << "Failed to save the BMP image." << std::endl;
         }
     }
+
+
+    bool loadFromBMP(const char* filename, std::vector<std::tuple<int, int, int>>& pixels, int& width, int& height) {
+        // Load image
+        int channels;
+        uint8_t* imgData = stbi_load(filename, &width, &height, &channels, 0);
+
+        if (!imgData) {
+            std::cerr << "Failed to load the BMP image." << std::endl;
+            return false;
+        }
+
+        // Ensure the image has 3 channels (RGB)
+        if (channels != 3) {
+            std::cerr << "Image is not in RGB format." << std::endl;
+            stbi_image_free(imgData);
+            return false;
+        }
+
+        // Convert the loaded image data into the vector tuple format of ints
+        pixels.clear();
+        for (int y = 0; y < height; ++y) {
+            for (int x = 0; x < width; ++x) {
+                // Grab index of curr pixel
+                int index = (y * width + x) * 3;
+                uint8_t b = imgData[index];     // B
+                uint8_t g = imgData[index + 1]; // G
+                uint8_t r = imgData[index + 2]; // R
+                pixels.push_back(std::make_tuple(static_cast<int>(r), static_cast<int>(g), static_cast<int>(b)));
+            }
+        }
+
+        stbi_image_free(imgData);
+        return true;
+    }
+}
+
+namespace denoise {
+    // Grab squared distance between two colour values
+    float colDisSquared(const std::tuple<int, int, int>& c1, const std::tuple<int, int, int>& c2) {
+        float rDiff = std::get<0>(c1) - std::get<0>(c2);
+        float gDiff = std::get<1>(c1) - std::get<1>(c2);
+        float bDiff = std::get<2>(c1) - std::get<2>(c2);
+        return rDiff * rDiff + gDiff * gDiff + bDiff * bDiff;
+    }
+
+    // Compute weight for a neighboring pixel (m, n) given distance and smoothing param
+    float computeWeight(float distance, float h) {
+        return std::exp(-distance / (h * h));
+    }
+
+    // Extract a patch centered on (i, j) in the unfiltered image
+    std::vector<std::tuple<int, int, int>> extractPatch(const std::vector<std::tuple<int, int, int>>& pixels,
+                                                        int width,
+                                                        int height,
+                                                        int i,
+                                                        int j,
+                                                        int patchRadius) {
+        std::vector<std::tuple<int, int, int>> patch;
+        
+        // Loop through pixel kernel
+        // For pixels on the image edge, we clamp and pad the image by stretching the edge pixel
+        for (int m = -patchRadius; m <= patchRadius; ++m) {
+            for (int n = -patchRadius; n <= patchRadius; ++n) {
+                int ni = std::clamp(i + m, 0, height - 1);
+                int nj = std::clamp(j + n, 0, width - 1);
+                patch.push_back(pixels[ni * width + nj]);
+            }
+        }
+        return patch;
+    }
 }
 
 class MainWindow : public QMainWindow {
@@ -239,13 +310,16 @@ public:
     int imageHeight;
     int sampleCount;
     std::string sceneName;
+    std::string renderFileName;
+    std::string renderFilePath;
     bool useMultithread;
 
     QLineEdit* widthInput;
     QLineEdit* heightInput;
     QLineEdit* sampleCountInput;
     QButtonGroup* sceneGroup;
-    QLabel* filePathLabel;
+    QLabel* sceneFilePathLabel;
+    QLabel* renderFilePathLabel;
     QRadioButton* singleThreadedButton;
     QRadioButton* multiThreadedButton;
 
@@ -338,22 +412,41 @@ public:
         QVBoxLayout* txtLayout = new QVBoxLayout(userSceneTab);
 
         QPushButton* loadFileButton = new QPushButton("Load Scene File");
-        filePathLabel = new QLabel("No file selected.");
+        sceneFilePathLabel = new QLabel("No file selected.");
         connect(loadFileButton, &QPushButton::clicked, this, &MainWindow::openLoadedScene);
 
         QPushButton* startRenderButtonLoaded = new QPushButton("Start Render");
         txtLayout->addWidget(new QLabel("Load Custom Scene:"));
         txtLayout->addWidget(loadFileButton);
-        txtLayout->addWidget(filePathLabel);
+        txtLayout->addWidget(sceneFilePathLabel);
         txtLayout->addWidget(startRenderButtonLoaded);
 
         // Connect tabs and buttons
         connect(startRenderButtonPremade, &QPushButton::clicked, this, &MainWindow::startPremadeRender);
         connect(startRenderButtonLoaded, &QPushButton::clicked, this, &MainWindow::startLoadedRender);
 
+        // Tab 3: Load Scene from JSON
+        QWidget* denoiseTab = new QWidget();
+        QVBoxLayout* denoiseLayout = new QVBoxLayout(denoiseTab);
+
+        QPushButton* loadRenderButton = new QPushButton("Load Render Output");
+        renderFilePathLabel = new QLabel("No file selected.");
+        connect(loadRenderButton, &QPushButton::clicked, this, &MainWindow::openLoadedRender);
+
+        QPushButton* startDenoiseButton = new QPushButton("Start Denoise");
+        denoiseLayout->addWidget(new QLabel("Load Render Output (BMP):"));
+        denoiseLayout->addWidget(loadRenderButton);
+        denoiseLayout->addWidget(renderFilePathLabel);
+        denoiseLayout->addWidget(startDenoiseButton);
+
+        // Connect tabs and buttons
+        connect(startDenoiseButton, &QPushButton::clicked, this, &MainWindow::startDenoise);
+
+        // Add tabs to window
         tabWidget->addTab(inputTab, "Render Settings");
         tabWidget->addTab(premadeSceneTab, "Premade Scenes");
         tabWidget->addTab(userSceneTab, "Load Scene");
+        tabWidget->addTab(denoiseTab, "Denoise Render");
 
         // Set central widget
         setCentralWidget(tabWidget);
@@ -520,9 +613,120 @@ private slots:
         if (!filePath.isEmpty()) {
             QFileInfo fileInfo(filePath);
             QString fileName = fileInfo.fileName();
-            filePathLabel->setText(fileName);
+            sceneFilePathLabel->setText(fileName);
             sceneName = fileName.toUtf8().constData();
         }
+    }
+
+    void openLoadedRender() {
+        QString filePath = QFileDialog::getOpenFileName(this, "Open Render File", "", "BMP Files (*.bmp)");
+        if (!filePath.isEmpty()) {
+            QFileInfo fileInfo(filePath);
+            QString fileName = fileInfo.fileName();
+            QString filePath = fileInfo.absoluteFilePath();
+            renderFilePathLabel->setText(fileName); 
+            renderFileName = fileName.toUtf8().constData();
+            renderFilePath = filePath.toUtf8().constData();
+        }
+    }
+
+    void startDenoise() {
+        // Load BMP file into pixel array
+        std::vector<std::tuple<int, int, int>> unfilteredPixels;
+        std::vector<std::tuple<int, int, int>> filteredPixels;
+        int width, height;
+
+        if (render::loadFromBMP(renderFilePath.c_str(), unfilteredPixels, width, height)) {
+            std::cout << "Image loaded successfully!" << std::endl;
+            std::cout << "Width: " << width << ", Height: " << height << std::endl;
+        } else {
+            std::cout << "Failed to load the image." << std::endl;
+            return;
+        }
+
+        filteredPixels.resize(width * height);
+
+        /*
+        We use the techniques in the paper "Nonlinearly Weighted First-order Regression for
+        Denoising Monte Carlo Renderings" to denoise our monte-carlo render. 
+        https://benedikt-bitterli.me/nfor/
+
+        We do so by have two passes. 
+        First pass calculates pixel weights using non-local means then uses first-order 
+        regression to compute denoised value.
+        Second pass uses variance of first pass as regression weights, doing the same
+        first-order regression to remove residual artefacts. 
+        */
+
+        const int patchRadius = 2; // 3x3
+        const float smoothing = 10.0f; 
+
+        /// FIRST PASS
+        // Calculate weights using non-local means
+        for (int i = 0; i < height; ++i) {
+            for (int j = 0; j < width; ++j) {
+                // Grab patch at pixel (i, j)
+                std::vector<std::tuple<int, int, int>> currPatch = denoise::extractPatch(unfilteredPixels, 
+                                                                                         width,
+                                                                                         height,
+                                                                                         i,
+                                                                                         j,
+                                                                                         patchRadius);
+                // Sums of colour channels
+                float rSum = 0.0f;
+                float gSum = 0.0f;
+                float bSum = 0.0f;
+                float weightSum = 0.0f;
+                
+                // Loop neighbours and calculate their weights, then accumulate weight per pixel
+                for (int m = -patchRadius; m <= patchRadius; ++m) {
+                    for (int n = -patchRadius; n <= patchRadius; ++n) {
+                        int ni = std::clamp(i + m, 0, height - 1);
+                        int nj = std::clamp(j + n, 0, width - 1);
+                        std::vector<std::tuple<int, int, int>> currPatch = denoise::extractPatch(unfilteredPixels,
+                            width,
+                            height,
+                            ni,
+                            nj,
+                            patchRadius);
+
+                        // Compute weight #TODO add equation here
+                        float currDis = 0.0f;
+                        for (size_t k = 0; k < currPatch.size(); ++k) {
+                            currDis += denoise::colDisSquared(currPatch[k], currPatch[k]);
+                        }
+                        float currWeight = denoise::computeWeight(currDis, smoothing);
+                        weightSum += currWeight;
+
+                        // Accumulate weighted colour value
+                        std::tuple<int, int, int>& origCol = unfilteredPixels[ni * width + nj];
+                        rSum += currWeight * std::get<0>(origCol);
+                        gSum += currWeight * std::get<1>(origCol);
+                        bSum += currWeight * std::get<2>(origCol);
+                    }
+                }
+
+                // Normalize and add weights
+                rSum /= weightSum;
+                gSum /= weightSum;
+                bSum /= weightSum;
+                filteredPixels[i * width + j] = std::make_tuple(static_cast<int>(bSum), 
+                                                                static_cast<int>(gSum), 
+                                                                static_cast<int>(rSum));
+            }
+        }
+
+        /// SECOND PASS
+
+
+        // Save final image
+        std::string pathCopy = renderFileName;
+        std::string suffix = "_denoised";
+        size_t dotPos = pathCopy.find_last_of('.');
+        pathCopy.insert(dotPos, suffix);
+
+        render::saveToBMP(pathCopy.c_str(), width, height, filteredPixels);
+        std::cout << "Finished and exported to " << pathCopy << std::endl;
     }
 };
 
