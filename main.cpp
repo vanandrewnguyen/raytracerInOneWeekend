ac274ea86db71639e8b0555244c3d940ca278899
Van Andrew <andrew.nguyen25@outlook.com>
2024-11-14 16:59:20 +1100

    Updated final renders.

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
     MainWindow window;
     window.setWindowTitle("Toy Raytracer");
-    window.resize(600, 200);
+    window.resize(800, 400);
     window.show();
     return app.exec();
 }
